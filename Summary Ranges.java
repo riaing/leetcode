d8fc32b5f8c428b0b7f6d32527fcfcb677bc3b53
riaing <panjfang@gmail.com>
2018-08-20 23:35:14 -0700

    Create Summary Ranges.java

---
+++ Summary Ranges.java
@@ -0,0 +1,60 @@
+Given a sorted integer array without duplicates, return the summary of its ranges.
+
+Example 1:
+
+Input:  [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: 0,1,2 form a continuous range; 4,5 form a continuous range.
+Example 2:
+
+Input:  [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: 2,3,4 form a continuous range; 8,9 form a continuous range. 
+
+--------------solution 1, while loop with several pointer  -------------
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> results = new ArrayList<String>();
+        if (nums == null || nums.length == 0) {
+            return results; 
+        }
+
+        int start = 0;
+        int end = start + 1;
+        int temStart = start; 
+        while(temStart < nums.length && end < nums.length) {
+            long endNum = nums[end];
+            long startNum = nums[temStart];
+            if (endNum - startNum == 1) {
+                System.out.println("here");
+                end ++;
+                temStart++;
+                //break;
+            }
+            else if(endNum - startNum > 1) {
+                System.out.println("here2" );
+                if (start == temStart) {
+                    results.add(Integer.toString(nums[start]));
+                }
+                else{
+                    String range = nums[start] + "->" + nums[temStart];
+                    results.add(range);
+                }
+                temStart = end;
+                
+                start = temStart;
+              
+                end ++;
+            }
+        }
+        if (start == nums.length -1) {
+            results.add(Integer.toString(nums[start]));
+        }
+        else{
+            String range = nums[start] + "->" + nums[temStart];
+                    results.add(range);
+        }
+        return results;
+    }
+}
+
