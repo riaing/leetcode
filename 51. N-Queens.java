45b5d09eebcbed56643ce61e0f80bb36e416d47b
riaing <panjfang@gmail.com>
2019-03-05 23:45:09 -0800

    Update 51. N-Queens.java

--- 51. N-Queens.java
+++ 51. N-Queens.java
@@ -5,7 +5,7 @@ The n-queens puzzle is the problem of placing n queens on an n×n chessboard suc
 
 Given an integer n, return all distinct solutions to the n-queens puzzle.
 
-Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+Each solution contains a distinct board configuration of the n-queens placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
 
 Example:
 
