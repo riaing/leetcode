e27dedb9efc91703ecc69cf5353424ddff9012fc
riaing <panjfang@gmail.com>
2019-03-14 17:52:43 -0700

    Update 698. Partition to K Equal Sum Subsets.java

--- 698. Partition to K Equal Sum Subsets.java
+++ 698. Partition to K Equal Sum Subsets.java
@@ -65,3 +65,55 @@ class Solution {
         return false;
     }
 }
+-------------基础版升级，排序加剪枝----------------------------------
+ /**
+比如先给数组按从大到小的顺序排个序，然后在递归函数中，我们可以直接判断，如果curSum大于target了，直接返回false，因为题目中限定了都是正数，并且我们也给数组排序了，后面的数字只能更大，这个剪枝操作大大的提高了运行速度
+
+time: o(n* n^2)? 
+**/
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        Arrays.sort(nums);
+        //find the average num of this list, all subsets must add up to this ave
+        int sum = 0;
+        for(int num : nums) {
+            sum += num;
+        }
+        // if it's not divisible, return false; 
+        if (sum % k != 0) {
+            return false;
+        }
+        int ave = sum / k;
+        
+        boolean[] visited = new boolean[nums.length];
+        return search(0, 0, ave, k, nums, visited);  
+    }
+    
+    private boolean search(int curSum, int start, int target, int k, int[] nums, boolean[] visited) {
+        // prune
+        if(curSum > target) {
+            return false;
+        }
+        
+        if (k == 1) {
+            return true;
+        }
+     
+        // if we found we subset, we need to search for another subset, by reset curSum to 0, start to 0(means we will search from beginning again), and k-1(means we've already found one subset)
+        if (curSum == target) {
+            return search(0, 0, target, k-1, nums, visited);
+        }
+        //if we don't find one subset, we continue by adding numbers to current subset
+        for (int i = start; i < nums.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            if (search(curSum+nums[i], i+1, target, k, nums, visited)) {
+                return true;
+            }
+            visited[i] = false;
+        }
+        return false;
+    }
+}
