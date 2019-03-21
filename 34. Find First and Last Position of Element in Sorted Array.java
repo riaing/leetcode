0fc08153c2eaaf45608d7a82a0bd762e0dc71a0e
riaing <panjfang@gmail.com>
2019-03-20 22:41:03 -0700

    Update 34. Find First and Last Position of Element in Sorted Array.java

--- 34. Find First and Last Position of Element in Sorted Array.java
+++ 34. Find First and Last Position of Element in Sorted Array.java
@@ -1,6 +1,6 @@
 Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
 
-Your algorithm's runtime complexity must be in the order of O(log n).
+Your algorithm’s runtime complexity must be in the order of O(log n).
 
 If the target is not found in the array, return [-1, -1].
 
