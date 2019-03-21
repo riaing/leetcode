9ccf878520fd2b32491cb792384dfc7e47de6473
riaing <panjfang@gmail.com>
2019-03-20 22:46:59 -0700

    Update 35. Search Insert Position.java

--- 35. Search Insert Position.java
+++ 35. Search Insert Position.java
@@ -53,3 +53,52 @@ class Solution {
         }
     }
 }
+------------------------最基本的binary search写法------------------------------------------------------------------------------
+    Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
+
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+    
+   ----------写法
+   class Solution {
+    public int search(int[] nums, int target) {
+         int start = 0;
+        int end = nums.length-1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                start = mid;
+            }
+            if (nums[mid] > target) {
+                end = mid;
+            }
+        }
+        // consider all conditions: 
+        //1, add to beginning 
+        if (nums[start] == target) {
+            return start;
+        }
+        // 2, add to middle
+        else if (nums[end] == target) {
+            return end;
+        }
+        // 3, not exist
+        // if (nums[end] < target)
+        else {
+            return -1;
+        }
+    }
+}
