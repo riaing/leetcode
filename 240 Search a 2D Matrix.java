8480ed31ccb87f7bb3fbaa8a88bb1557e09e261d
riaing <panjfang@gmail.com>
2019-03-22 11:41:41 -0700

    Update 240 Search a 2D Matrix.java

--- 240 Search a 2D Matrix.java
+++ 240 Search a 2D Matrix.java
@@ -62,3 +62,58 @@ public class Solution {
         
     }
 }
+----------------------3.22.19 with 九章模板------------------------------------------------------------------------
+  解法：先二分行，找到可能在哪一行，再二分列
+  Time: ROW: O(logm). COL: O(logn) --> O(logm) + O(logn) = O (log(m*n))
+  class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // [], [[]] as corner case 
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false; 
+        }
+        int row = findRow(matrix, target);
+        
+        // binery search in this row to find if target exists
+        return findInCol(matrix[row], target);
+        
+    }
+    private int findRow(int[][] matrix, int target) {
+        int start = 0;
+        int end = matrix.length -1;
+        while(start + 1 < end) {
+            int mid = start + (end-start) / 2;
+            if (matrix[mid][0] <= target) {
+                start = mid;
+            }
+            else {
+                end = mid;
+            }
+        }
+        //如果end这一行的开头已经大于target了，那么target肯定不在这一行
+       
+        if (matrix[end][0] > target) {
+            return start;
+        } 
+        else {
+            return end;
+        }
+    }
+    
+    private boolean findInCol(int[] row, int target) {
+        int start = 0;
+        int end = row.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (row[mid] == target) {
+                return true;
+            }
+            else if (row[mid] > target) {
+                end = mid;
+            }
+            else {
+                start = mid;
+            }
+        }
+        return row[end] == target || row[start] == target;
+    }
+}
