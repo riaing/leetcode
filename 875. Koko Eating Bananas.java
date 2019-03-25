83124006951a5f5427004798cd3a4078644f8094
riaing <panjfang@gmail.com>
2019-03-24 23:34:42 -0700

    Update 875. Koko Eating Bananas.java

--- 875. Koko Eating Bananas.java
+++ 875. Koko Eating Bananas.java
@@ -1,6 +1,9 @@
-Koko loves to eat bananas.  There are N piles of bananas, the i-th pile has piles[i] bananas.  The guards have gone and will come back in H hours.
+Koko loves to eat bananas.  There are N piles of bananas, the i-th pile has piles[i] bananas.  The guards have gone and 
+will come back in H hours.
 
-Koko can decide her bananas-per-hour eating speed of K.  Each hour, she chooses some pile of bananas, and eats K bananas from that pile.  If the pile has less than K bananas, she eats all of them instead, and won't eat any more bananas during this hour.
+Koko can decide her bananas-per-hour eating speed of K.  Each hour, she chooses some pile of bananas, and eats K bananas
+from that pile.  If the pile has less than K bananas, she eats all of them instead, and won’t eat any more bananas during 
+this hour.
 
 Koko likes to eat slowly, but still wants to finish eating all the bananas before the guards come back.
 
@@ -30,7 +33,8 @@ piles.length <= H <= 10^9
 
 ---------------------------------------------------------------------------------------------------------------------
 /**
-bineray search, 关键是确定min和max，我们猜hour，min=1-总要吃；max的话，因为题目中说吃完一串后那个小时就不动了，所以max可以是piles中最大的数。根据模板，我们再把max+1
+bineray search, 关键是确定min和max，我们猜hour，min=1-总要吃；max的话，因为题目中说吃完一串后那个小时就不动了，所以max可以是piles中最大的数。
+根据模板，我们再把max+1
 
 time:O(n*logW) W-> W is the maximum size of a pile.
 space: O(1)
