5ccb5ecd9a7f15dcc5649d6e403b67fd44632e7a
riaing <panjfang@gmail.com>
2019-03-27 21:27:20 -0700

    Update 94. Binary Tree Inorder Traversal.java

--- 94. Binary Tree Inorder Traversal.java
+++ 94. Binary Tree Inorder Traversal.java
@@ -34,3 +34,35 @@ class Solution {
         return res;
     }
 }
+
+----------------------非递归写法，用stack-----------------------------------------------------------------------------
+   /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Deque<TreeNode> s = new LinkedList<TreeNode>();
+        
+        TreeNode cur = root;
+        while (!s.isEmpty() || cur != null) {
+            // 若其左孩子不为空，则将P入栈并将P的左孩子置为当前的P，然后对当前结点P再进行相同的处理；
+            while (cur != null) {
+                s.push(cur);
+                cur = cur.left;
+            }
+            // 若其左孩子为空，则取栈顶元素并进行出栈操作，访问该栈顶结点，然后将当前的P置为栈顶结点的右孩子；
+            TreeNode tmp = s.pop();
+            res.add(tmp.val);
+            cur = tmp.right;
+        }
+        // 直到P为NULL并且栈为空则遍历结束
+        return res;
+    }
+}
