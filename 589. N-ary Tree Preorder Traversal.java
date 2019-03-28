319ad3c1e1c4edec0a49b06abe99c5ce799b18de
riaing <panjfang@gmail.com>
2019-03-27 23:48:45 -0700

    Update 589. N-ary Tree Preorder Traversal.java

--- 589. N-ary Tree Preorder Traversal.java
+++ 589. N-ary Tree Preorder Traversal.java
@@ -1,4 +1,4 @@
-Given an n-ary tree, return the preorder traversal of its nodes' values.
+Given an n-ary tree, return the preorder traversal of its nodes’ values.
 
 For example, given a 3-ary tree:
 
@@ -36,4 +36,40 @@ class Solution {
         }
         return res;
     }
-}`1
+}
+
+-------------------------------非递归stack---------------------------------------------------------------------
+ /*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        Deque<Node> s = new LinkedList<Node>();
+        s.push(root);
+        while(!s.isEmpty()) {
+            Node cur = s.pop();
+            if (cur != null) {
+                res.add(cur.val);
+                for (int i = cur.children.size()-1; i >= 0; i--) { //题目中定义的是从左到右，所以这里反向加入stack
+                    s.push(cur.children.get(i));
+                }
+            }
+        }
+        return res;
+    }
+}
