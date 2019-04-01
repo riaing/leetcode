d0565228d6dfac5152c283a91de5c6a49327744a
riaing <panjfang@gmail.com>
2019-03-31 21:16:01 -0700

    Create 236. Lowest Common Ancestor of a Binary Tree.java

---
+++ 236. Lowest Common Ancestor of a Binary Tree.java
@@ -0,0 +1,103 @@
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+
+
+ 
+
+Example 1:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+Example 2:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+ 
+
+Note:
+
+All of the nodes' values will be unique.
+p and q are different and both values will exist in the binary tree.
+
+------------------------------------------- D&C  O(n) -----------------------------------------------------------------------------
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        
+        if (p == root || q == root) {
+            return root;
+        }
+        
+        TreeNode left_ancestor = lowestCommonAncestor(root.left, p, q);
+        TreeNode right_ancestor = lowestCommonAncestor(root.right, p, q);
+        if (left_ancestor != null && right_ancestor != null) {
+            return root;
+        } else if (left_ancestor != null) {
+            return left_ancestor;
+        } else if (right_ancestor != null) {
+            return right_ancestor;
+        } else {
+            return null;
+        }
+    }
+}
+
+------------------------------------ brute force  o(n^2)--------------------------------------------------------------------------------------
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+        
+        if (p == root || q == root) {
+            return root;
+        }
+        boolean pAtLeft = contains(root.left, p);
+        boolean qAtLeft = contains(root.left, q);
+        
+        if (pAtLeft && qAtLeft) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        else if (!pAtLeft && !qAtLeft) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        else {
+            return root;
+        }
+    }
+    
+    private boolean contains(TreeNode root, TreeNode node) {
+        if (root == null) {
+            return false;
+        }
+        if (root == node) {
+            return true;
+        }
+        return contains(root.left, node) || contains(root.right, node); 
+    }
+}
