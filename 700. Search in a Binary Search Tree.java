5facea11aeddc16b4d792f73e9c8f4b31954debf
riaing <panjfang@gmail.com>
2019-04-03 15:33:32 -0700

    Create 700. Search in a Binary Search Tree.java

---
+++ 700. Search in a Binary Search Tree.java
@@ -0,0 +1,49 @@
+Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+For example, 
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+And the value to search: 2
+You should return this subtree:
+
+      2     
+     / \   
+    1   3
+In the example above, if we want to search the value 5, since there is no node with value 5, we should return NULL.
+
+Note that an empty tree is represented by NULL, therefore you would see the expected output (serialized tree format) as [], not null.
+
+---------------相当于根据BST的性质Binary search----------------------------------------------------------------------------------
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// Based on the BST idea, we traverse to the subtree of the original BST based on the property that root.left < root < root.right.
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        else if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+        else { // if (root.val < val) 
+            return searchBST(root.right, val);
+        }
+       
+    }
+}
