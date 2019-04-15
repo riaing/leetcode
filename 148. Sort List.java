7df7e24ac98f463a88d5c44d2d49bd67c9ff90c4
riaing <panjfang@gmail.com>
2019-04-14 23:42:45 -0700

    Update 148. Sort List.java

--- 148. Sort List.java
+++ 148. Sort List.java
@@ -8,7 +8,7 @@ Example 2:
 
 Input: -1->5->3->4->0
 Output: -1->0->3->4->5
------------merge sort写法------------------------------------------------------------------------------------------------
+--------------------------merge sort写法， 找中点，合并两个sorted list------------------------------------------------------------------------------------------------
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -56,5 +56,90 @@ class Solution {
             head.next = mergeTwoSortedList(part1, part2.next);
         }
         return head;
+    }
+}
+
+-------------------------quick sort 写法：找中点，partition，合并----------------------------------------------------------------------------------
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // 1, 找中点
+        ListNode middle = findMiddle(head);
+        // 2, 根据中点partition成两个不包括中点node的list，partition[0]为值小于等于中点的，[1]大于中点的.注意这里考虑了重复元素
+        ListNode[] partition = partitionList(head, middle);
+        ListNode small = sortList(partition[0]);
+        ListNode large = sortList(partition[1]);
+        
+        //3， 合并。
+        // a, 左边没值，直接中点接上右边。 
+        // b, 右边每值，找到左边最后node，接上右边
+        // c, 两边都有值，左边最后node->中点->右边
+        if (small == null) {
+            middle.next = large;
+            return middle;
+        }
+ 
+        else{
+            // find the last element of small 
+             ListNode smallEnd = small;
+            while (smallEnd.next != null) {
+                smallEnd = smallEnd.next;
+            }
+            smallEnd.next = middle;
+            if (large == null) {
+                middle.next = null;
+            }
+            else {
+                middle.next = large;
+            }
+           
+            return small;
+        }
+    }
+    
+    private ListNode findMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
+    // 和普通partition时，quick sort的partition不包括本元素
+    private ListNode[] partitionList(ListNode head, ListNode node) {
+        ListNode dummySmall = new ListNode(0);
+        ListNode small = dummySmall;
+        ListNode dummyLarge = new ListNode(0);
+        ListNode large = dummyLarge;
+        while (head != null) {
+            if (head.val <= node.val && head != node) { //重复值的node加进小的一边
+                small.next = head;
+                small = small.next;
+            }
+            else if (head.val > node.val) {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        // 必须要记得断掉！
+        small.next = null;
+        large.next = null;
+        return new ListNode[]{dummySmall.next, dummyLarge.next};
     }
 }
