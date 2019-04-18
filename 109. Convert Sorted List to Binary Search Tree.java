e3d17b69dd11eaecc21af9b05b138ad277ebd269
riaing <panjfang@gmail.com>
2019-04-17 21:20:58 -0700

    Update 109. Convert Sorted List to Binary Search Tree.java

--- 109. Convert Sorted List to Binary Search Tree.java
+++ 109. Convert Sorted List to Binary Search Tree.java
@@ -95,4 +95,74 @@ public class Solution {
     }
 }
 -------------------同样思路优化，但不用global var，定义一个class来存treeNode和下一个要访问的listNode----------------------------------
-  思路同上
+  思路同上，但用class来记录下一个需要访问的listNode，可以想像有一个隐形的pointer被这个class携带。time,space同上
+  
+  /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/**
+从头开始，先处理size/2个node，形成左子树，然后pointer移到下一个node，作为root，然后处理root.next 开头的长度为size - 1 - size/2的list，
+作为右子树。这样的话每个node只访问了一遍，时间是o(n), 空间是O（lgn）stack space。
+*/
+class Solution {
+    
+    class Node {
+        TreeNode _root;
+        ListNode _nextHead; 
+        
+        Node(TreeNode root, ListNode nextHead) {
+            _root = root;
+            _nextHead = nextHead; 
+        }
+    }
+    
+    public TreeNode sortedListToBST(ListNode head) {
+        // find the size of the list 
+        int size = 0; 
+        ListNode cur = head; 
+        while (cur != null) {
+            size ++;
+            cur = cur.next;
+        }
+        Node root = helper(head, size);
+        return root._root;
+    }
+    
+    private Node helper(ListNode head, int size) {
+        if (size <= 0) {
+            return new Node(null, head);
+        }
+        
+         if (size == 1) {
+            TreeNode leave = new TreeNode(head.val);
+            return new Node(leave, head.next);
+        }
+        
+        Node leftNode = helper(head, size / 2);
+        Node rightNode = helper(leftNode._nextHead.next, size - 1 - size / 2); 
+      
+        TreeNode treeRoot = new TreeNode(leftNode._nextHead.val);
+       
+        treeRoot.left = leftNode._root;
+        treeRoot.right = rightNode._root;
+        
+        Node root = new Node(treeRoot, rightNode._nextHead);
+        return root;
+    }
+}
+  
+  
