48309fc49b030fe681ca63b135b588f7db323094
riaing <panjfang@gmail.com>
2019-04-17 23:59:27 -0700

    Update Add Two Numbers.java

--- Add Two Numbers.java
+++ Add Two Numbers.java
@@ -55,3 +55,52 @@ public class Solution {
         return dum.next;  //if there is point left 
     }
 }
+
+------------------------4.17.19 update,省成一个while --------------------------------------------------------------------
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        boolean carry = false;
+        ListNode dummy = new ListNode(0);
+        ListNode tmp = dummy;
+        
+        //只要任何一个有数都能进此循环
+        while (l1 != null || l2 != null) {
+            int cur = (l1 == null ? 0: l1.val) + (l2 == null ? 0 : l2.val) + (carry ? 1 : 0);
+            carry = cur / 10 != 0;
+            cur = cur % 10;
+            tmp.next = new ListNode(cur);
+            tmp = tmp.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }  
+        }
+        
+        // 上面的while从 && 改成||后，这段就不用了
+        // if (l1 != null || l2 != null) {
+        //     ListNode remain = l1 == null ? l2 : l1;
+        //     while (remain != null) {
+        //         int cur = remain.val + (carry ? 1 : 0);
+        //         carry = cur / 10 != 0;
+        //         cur = cur % 10;
+        //         tmp.next = new ListNode(cur);
+        //         tmp = tmp.next;
+        //         remain = remain.next;
+        //     }
+        // }
+        if (carry) {
+            tmp.next = new ListNode(1);
+        }
+        return dummy.next;
+    }
+}
