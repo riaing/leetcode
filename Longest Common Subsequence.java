9e54bfc1d42a4e9ed65a2b7b9cea3a20e5641f0f
riaing <panjfang@gmail.com>
2019-04-24 17:36:36 -0700

    Create Longest Common Subsequence.java

---
+++ Longest Common Subsequence.java
@@ -0,0 +1,76 @@
+Given two strings, find the longest common subsequence (LCS).
+
+Your code should return the length of LCS.
+
+Example
+Example 1:
+	Input:  "ABCD" and "EDCA"
+	Output:  1
+	
+	Explanation:
+	LCS is 'A' or  'D' or 'C'
+
+
+Example 2:
+	Input: "ABCD" and "EACB"
+	Output:  2
+	
+	Explanation: 
+	LCS is "AC"
+Clarification
+What's the definition of Longest Common Subsequence?
+
+https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
+http://baike.baidu.com/view/2020307.htm
+
+-----------------------DP----------------------------------------------------------------------------------------------
+public class Solution {
+    /**
+     * @param A: A string
+     * @param B: A string
+     * @return: The length of longest common subsequence of A and B
+     */
+    public int longestCommonSubsequence(String A, String B) {
+        int[][] m = new int[A.length()+1][B.length()+1];
+        
+        for (int i = 1; i <= A.length(); i++) {
+            for (int j = 1; j <= B.length(); j++) {
+                if (A.charAt(i-1) == B.charAt(j-1)) {
+                    m[i][j] = m[i-1][j-1] + 1;
+                }
+                else {
+                     m[i][j] = Math.max(m[i][j-1], m[i-1][j]);
+                }
+            }
+        }
+        return m[A.length()][B.length()];
+    }
+}
+
+--------------DP with save space ----------------------------------------------------------------------------
+因为只和i-1上一行相关，所以用rotational array的方法
+public class Solution {
+    /**
+     * @param A: A string
+     * @param B: A string
+     * @return: The length of longest common subsequence of A and B
+     */
+    public int longestCommonSubsequence(String A, String B) {
+        int[][] m = new int[2][B.length()+1];
+        
+        int oldrow = 0; 
+        for (int i = 1; i <= A.length(); i++) {
+            int newRow = 1 - oldrow;
+            for (int j = 1; j <= B.length(); j++) {
+                if (A.charAt(i-1) == B.charAt(j-1)) {
+                    m[newRow][j] = m[oldrow][j-1] + 1;
+                }
+                else {å
+                     m[newRow][j] = Math.max(m[newRow][j-1], m[oldrow][j]);
+                }
+            }
+            oldrow = newRow;
+        }
+        return m[oldrow][B.length()];
+    }
+}
