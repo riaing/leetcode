779ce7fec585612ac4483c6d4cd8f3a463bad5e6
riaing <panjfang@gmail.com>
2019-06-10 23:57:25 -0700

    Update 679. 24 Game.java

--- 679. 24 Game.java
+++ 679. 24 Game.java
@@ -1,3 +1,20 @@
+You have 4 cards each containing a number from 1 to 9. You need to judge whether they could operated through *, /, +, -, (, ) 
+    to get the value of 24.
+
+Example 1:
+Input: [4, 1, 8, 7]
+Output: True
+Explanation: (8-4) * (7-1) = 24
+Example 2:
+Input: [1, 2, 1, 2]
+Output: False
+Note:
+The division operator / represents real division, not integer division. For example, 4 / (1 - 2/3) = 12.
+Every operation done is between two numbers. In particular, we cannot use - as a unary operator. For example, with [1, 1, 1, 1] as 
+input, the expression -1 - 1 - 1 - 1 is not allowed.
+You cannot concatenate numbers together. For example, if the input is [1, 2, 1, 2], we cannot write this as 12 + 12.
+
+----------------------------------------------------------------------
 /*
 考虑回溯法，首先我们从集合A = {1 、2、3、4}中任意取出两个数，如取1、2，那么A = A - {1、2}，对取出来的两个数字分别进行不同的四则运算，1 + 2、1 - 2……，将结果加入A中，有{3、3、4}、{-1，3，4}等，通过这种方法，将四个数降为三个数，然后降为两个数……
 
