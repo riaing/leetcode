f2c4ed1fc8e649d9732465e3c1dc53a6642fd8d3
riaing <panjfang@gmail.com>
2021-04-04 15:43:11 -0700

    Update 583. Delete Operation for Two Strings.java

--- 583. Delete Operation for Two Strings.java
+++ 583. Delete Operation for Two Strings.java
@@ -1,3 +1,57 @@
+Given two strings word1 and word2, return the minimum number of steps required to make word1 and word2 the same.
+
+In one step, you can delete exactly one character in either string.
+
+ 
+
+Example 1:
+
+Input: word1 = "sea", word2 = "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
+Example 2:
+
+Input: word1 = "leetcode", word2 = "etco"
+Output: 4
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 500
+word1 and word2 consist of only lowercase English letters.
+
+-------                         4.4.2021 DP --------------------------------------------------------------
+/*
+dp[i][j] 以i j个char结尾的string的min steps =>注意这里初始化用了【length+1】，来使得初始值简单。
+
+dp[i][j] = min{dp[i-1][j], dp[i][j-1]} + 1 => 删stringA 还是删StringB，删除操作就得加1 
+          && then = min(dp[i][j], dp[i-1][j-1]) if char i == char j =>如果i,j相同，还得和i-1 j-1比一比
+*/
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length()+1][word2.length()+1];
+        dp[0][0] = 0;
+        for (int i = 1; i <= word1.length(); i++) {
+            dp[i][0] = i;
+        }
+          for (int i = 1; i <= word2.length(); i++) {
+            dp[0][i] = i;
+        }
+        
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+              
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else {
+                      dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + 1;
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
 -------------------------------------------DP,类似Edit Distance----------------------------------------------------
 /**
 m[i][j] : min deletion 对于string1的前i个字符和string2的前j个字符
