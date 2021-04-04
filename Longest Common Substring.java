ede4d78fbbf1da516bd7d2c1c35be548d434736a
riaing <panjfang@gmail.com>
2021-04-04 00:44:47 -0700

    Update Longest Common Substring.java

--- Longest Common Substring.java
+++ Longest Common Substring.java
@@ -1,6 +1,6 @@
 Given two strings, find the longest common substring.
 
-Return the length of it.
+1. Return the length of it. 2. return the lCS
 
 Example
 Example 1:
@@ -22,7 +22,48 @@ O(n x m) time and memory.
 
 Notice
 The characters in substring should occur continuously in original string. This is different with subsequence.
+		
+----------- 4.3.2021 DP思路一样，learning是print结果时，用一个变量记录lcs的index，在dp过程中更新此值--------------------------------------
+	
 
+public class Solution {
+    /**
+     * @param A: A string
+     * @param B: A string
+     * @return: the length of the longest common substring.
+     */
+    public int longestCommonSubstring(String A, String B) {
+        // dp[i][j] => A的第i个字符以及B的第j个字符结尾的LCS 
+        int[][] dp = new int[A.length()+1][B.length()+1];
+        
+        int lcs = 0;
+        int endIndex = 0; // 用一个变量记录最后的index，以便接下来通过lcs和此index print出lcs
+        for (int i = 1; i <= A.length(); i++) {
+   
+            for (int j = 1; j <= B.length(); j++) {
+                if (A.charAt(i-1) == B.charAt(j-1)) {//i,j代表第几个字符，所以index时要减1
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    // 这一步用于print最后的valude
+                    if (lcs < dp[i][j]) {
+                        lcs = dp[i][j];
+                        endIndex = i - 1; // 因为要的是index所以减1 
+                    }
+                }
+                else {
+                    dp[i][j] = 0;
+                }
+               
+            }
+           
+        }
+        // get the value of LCS, 通过上面的lcs endIndex和长度来找 
+        int startIndex = endIndex - lcs + 1;
+        String theLCSValue = A.substring(startIndex, endIndex+1);
+        System.out.println(theLCSValue);
+        return lcs; 
+    }
+}
+		
 ------------------------DP 思路---------------------------------------------------------
 
     m[i][j] : 在a中以第i个字符结尾，在b中以第j个字符结尾的LCS
