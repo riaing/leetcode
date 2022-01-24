eb5b5ef469ef0e6906d480b6e05ac161b3c8e2d4
riaing <panjfang@gmail.com>
2022-01-24 11:53:53 -0800

    Update 876. Middle of the Linked List.java

--- 876. Middle of the Linked List.java
+++ 876. Middle of the Linked List.java
@@ -43,3 +43,32 @@ class Solution {
         return slow; 
     }
 }
+---------------- 2022.1.24 写着丑点但 intuitive 的解法 -------------------------------- 
+  
+  /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return head; 
+        }
+        ListNode slow = head;
+        ListNode quick = head;
+        
+        while (quick.next != null && quick.next.next != null) {
+            slow = slow.next;
+            quick = quick.next.next;
+        }
+        
+        return quick.next == null ? slow : slow.next;
+        
+    }
+}                               
