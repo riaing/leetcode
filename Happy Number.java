517837ef9b0c5f95d7f40fa984c00decd744935b
riaing <panjfang@gmail.com>
2022-01-24 11:42:23 -0800

    Update Happy Number.java

--- Happy Number.java
+++ Happy Number.java
@@ -34,3 +34,56 @@ class Solution {
         return sum;
     }
 }
+
+------------------ 2022.1.24: fast slow pointers -----------------------------
+    /*
+求 cycle 得题可转换成 fast slow pointer。
+快指针每次计算两次，慢指针计算一次。当两指针数相同时说明 meet in loop。当任意指针数为1时说明 end。
+time：假设链表长 N，o(N)
+*/
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int quick = n;
+        
+        while (quick != 1) {
+            int quickNext = calculate(quick);
+            int quickNextNext = calculate(quickNext);
+           
+            if (quickNext == 1 || quickNextNext == 1) { // 快指针总是先计算，这里用快指针作为终止条件
+                return true;
+            }
+            
+            quick = quickNextNext;
+            slow = calculate(slow);
+            if (quick == slow) {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+    
+    //一：先转成 string
+    private int calculate(int n) {
+        String nString = String.valueOf(n); 
+        int result = 0; 
+        for (int i = 0; i < nString.length(); i++) {
+            int curPositionVal =  Character.getNumericValue(nString.charAt(i));
+            result += curPositionVal*curPositionVal;
+        }
+        return result; 
+    }
+    
+    // 二：更好的找数字位数的方法
+    private static int calculate2(int num) {
+        int sum = 0, digit;
+        while (num > 0) {
+          digit = num % 10; // 从个位找起
+          sum += digit * digit;
+          num /= 10; //
+        }
+        return sum;
+  }
+}
+    
