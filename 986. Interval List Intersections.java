898d23ac4f3e6fe770ad136e655c7df28c3be653
riaing <panjfang@gmail.com>
2022-01-25 23:00:05 -0800

    Create 986. Interval List Intersections.java

---
+++ 986. Interval List Intersections.java
@@ -0,0 +1,69 @@
+iven two lists of intervals, find the intersection of these two lists. Each list consists of disjoint intervals sorted on their start time.
+
+Examples:
+
+Input: arr1=[[1, 3], [5, 6], [7, 9]], arr2=[[2, 3], [5, 7]]
+Output: [2, 3], [5, 6], [7, 7]
+Explanation: The output list contains the common intervals between the two lists.
+Example 2:
+
+Input: arr1=[[1, 3], [5, 7], [9, 12]], arr2=[[5, 10]]
+Output: [5, 7], [9, 10]
+Explanation: The output list contains the common intervals between the two lists.
+
+Input: firstList = [[1,3],[5,9]], secondList = []
+Output: []
+ 
+
+Constraints:
+
+0 <= firstList.length, secondList.length <= 1000
+firstList.length + secondList.length >= 1
+0 <= starti < endi <= 109
+endi < starti+1
+0 <= startj < endj <= 109
+endj < startj+1
+
+
+-------------------------------------------------------------
+// 运用 merge interval 的思路：1）判断何时 merge 2）merge 完后怎么处理
+class Solution {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        int i = 0;
+        int j = 0;
+        List<int[]> result = new ArrayList<int[]>();
+        while (i < firstList.length && j < secondList.length) {
+             // 1. 判断 overlap by a.end >= b.start. 再取 overlap 部分 by max[start1, start2] min[end1, end2]
+            int[] a;
+            int[] b;
+            if (firstList[i][0] <= secondList[j][0]) {
+                a = firstList[i];
+                b = secondList[j];
+            }
+            else {
+                a = secondList[j];
+                b = firstList[i];
+            }
+            if (a[1] >= b[0]) {
+                int[] tmp = new int[]{Math.max(a[0], b[0]), Math.min(a[1], b[1])};
+                result.add(tmp);
+            }
+            
+            // 2.重点！move ponter forward by comparing end。因为 end 长的可能还和别的 interval 有 overlap。ex：A: [9,20] B[11,12] [12, 14], [15, 16...20]
+            if (firstList[i][1] <= secondList[j][1]) {
+                i++;
+            }
+            else {
+                j++;
+            }
+            
+        }
+        
+        //3. put result together 
+        int[][] results = new int[result.size()][2];
+        for(int n = 0; n < result.size(); n++) {
+            results[n] = result.get(n);
+        }
+        return results;
+    }
+}
