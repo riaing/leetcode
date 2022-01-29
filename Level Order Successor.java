244bd2a2160b19e5667a4c757c28fc98a95ca9e6
riaing <panjfang@gmail.com>
2022-01-29 11:54:38 -0800

    Update Level Order Successor.java

--- Level Order Successor.java
+++ Level Order Successor.java
@@ -1,3 +1,6 @@
+
+https://www.educative.io/courses/grokking-the-coding-interview/7nO4VmA74Lr 
+
 Given a binary tree and a node, find the level order successor of the given node in the tree. The level order successor is the node that appears right after the given node in the level order traversal.
 
 Example 1:
