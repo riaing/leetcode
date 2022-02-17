610b5d19fed0b6b30524f55804d19e86811c8daa
riaing <panjfang@gmail.com>
2022-02-17 00:52:34 -0800

    Update Course Schedule.java

--- Course Schedule.java
+++ Course Schedule.java
@@ -49,3 +49,60 @@ public class Solution {
         
     }
 }
+
+--------------- 2022.2.16 ------------------------
+    class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer> res = new ArrayList<Integer>();
+        // 1. initialize node map and in-degree map 
+        Map<Integer, List<Integer>> nodes = new HashMap<Integer,List<Integer>>();
+        Map<Integer, Integer> inDegrees = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numCourses; i++) {
+            nodes.put(i, new ArrayList<Integer>());
+            inDegrees.put(i, 0);
+        }
+        
+         // 2, build  
+        for (int i = 0; i < prerequisites.length; i++) {
+            int parent = prerequisites[i][0];
+            int child = prerequisites[i][1];
+            nodes.get(parent).add(child);
+            inDegrees.put(child, inDegrees.get(child) + 1);
+        }
+        
+        // 3. find sources (in-degree = 0)
+        Queue<Integer> sources = new LinkedList<Integer>();
+        for (Map.Entry<Integer, Integer> entry : inDegrees.entrySet()) {
+            if (entry.getValue() == 0) {
+                sources.offer(entry.getKey());
+            }
+        }
+        
+        //4. sort 
+        while (sources.size() > 0) {
+            int curSource = sources.poll();
+            //1. add to list 
+            res.add(curSource);
+            //2. decrese children nodes's indegree 
+            for (int child : nodes.get(curSource)) {
+                inDegrees.put(child, inDegrees.get(child)-1);
+                // 3. add next 0 degree node to queue 
+                if (inDegrees.get(child) == 0) {
+                    sources.offer(child);
+                }
+            }
+        }
+        // 5. detect cycle 
+        // way 1:
+        if (res.size() != numCourses){ // 或者查 indegree map 还有没有谁的 in degree > 0 
+            return false;
+        }
+        // way 2: 
+        for (int key : inDegrees.keySet()) {
+            if (inDegrees.get(key) != 0) {
+                return false; 
+            }
+        }
+        return true; 
+    }
+}
