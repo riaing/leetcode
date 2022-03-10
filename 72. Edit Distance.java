35bf44d1a521b6323e297ce1aa730f2a966e96ea
riaing <panjfang@gmail.com>
2022-03-09 22:14:15 -0800

    Update 72. Edit Distance.java

--- 72. Edit Distance.java
+++ 72. Edit Distance.java
@@ -99,3 +99,35 @@ class Solution {
         return m[oldRow][word2.length()];
     }
 }
+---------------------2022.3.9 ------------------------------
+    /*
+dp[i][j] 前 i，j 个元素能变换的最小次数。0说明不取
+dp[0][0] = 0; dp[0][j] = j 全删; dp[i][0] = i
+
+dp[i][j] = min {
+                1+ min(dp[i-1][j], dp[i][j-1]), 
+                dp[i-1][j-1] if i==j or dp[i-1][j-1] + 1 if i!=j
+                }
+*/
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[2][word2.length()+1]; // 前 i，j 个元素能变换的最小次数。0说明不取
+        for (int j = 1; j <= word2.length(); j++) {
+            dp[0][j] = j; 
+        }
+        
+        for (int i = 1; i <= word1.length(); i++) {
+            dp[i%2][0] = i;
+            for (int j = 1; j <= word2.length(); j++) {
+                dp[i%2][j] = 1 + Math.min(dp[(i-1)%2][j], dp[i%2][j-1]); // 删除或增加的 action
+                if (word1.charAt(i-1) == word2.charAt(j-1)) { // replace action: when i == j 
+                    dp[i%2][j] = Math.min(dp[i%2][j], dp[(i-1)%2][j-1]); 
+                }
+                else { // replace action: when i != j 
+                     dp[i%2][j] = Math.min(dp[i%2][j], 1 + dp[(i-1)%2][j-1]); 
+                }
+            }
+        }
+        return dp[word1.length()%2][word2.length()];
+    }
+}
