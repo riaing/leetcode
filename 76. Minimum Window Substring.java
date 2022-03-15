b7399f9b6a32ba248d13a7089d2ad71446ab4cfc
riaing <panjfang@gmail.com>
2022-03-15 13:37:22 -0700

    Create 76. Minimum Window Substring.java

---
+++ 76. Minimum Window Substring.java
@@ -0,0 +1,82 @@
+Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+Example 2:
+
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+Example 3:
+
+Input: s = "a", t = "aa"
+Output: ""
+Explanation: Both 'a's from t must be included in the window.
+Since the largest window of s only has one 'a', return empty string.
+ 
+
+Constraints:
+
+m == s.length
+n == t.length
+1 <= m, n <= 105
+s and t consist of uppercase and lowercase English letters.
+
+------------------------- sliding window ----------------------------------------------------------------
+/*
+和https://leetcode.com/problems/find-all-anagrams-in-a-string/ 很像。同样是用map来记录pattern的frequency。
+这里维持window的条件是是找到一个解（map的value都为0）后，在window内更新result。而上题，维持window的条件是window size= pattern size、
+
+*/
+class Solution {
+    public String minWindow(String s, String t) {
+        
+        int start = 0; 
+        int countMatch = 0;
+        int subStringStart = s.length()-1; // 用来最后print结果
+        int minLen = s.length() + 1;  // something that's unreachable 
+        Map<Character, Integer> freq = new HashMap<Character, Integer>(); 
+        for (char c : t.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+        
+        for (int end = start; end < s.length(); end++) {
+            char endChar = s.charAt(end);
+            if (freq.containsKey(endChar)) { // decrement matching char 
+                freq.put(endChar, freq.get(endChar) - 1); 
+                if (freq.get(endChar) == 0) { // 等于0说明此char已经完全找到了，注意此value可能<0,就是说明找多了
+                    countMatch++; 
+                }
+            }
+            
+            // maintain a window of 找到一个解
+            while (countMatch == freq.size()) {
+                //说明找到了一个解，跟新一下参数
+                if(minLen > end - start + 1) {
+                    minLen = end - start + 1;
+                    subStringStart = start; 
+                }
+                // 把start往后移，并且更新countMatch
+                char startChar = s.charAt(start); 
+                if (freq.containsKey(startChar)) { 
+                    if (freq.get(startChar) == 0) { //如果没移前此char刚好match pattern的count，移完start后就不match pattern了
+                         countMatch--;
+                    }
+                     freq.put(startChar, freq.get(startChar) + 1);   
+                }
+                start++; 
+            }
+        }
+        // return "" if cannot find a result 
+        return minLen == s.length()+1 ? "" : s.substring(subStringStart, subStringStart + minLen); 
+    }
+}
