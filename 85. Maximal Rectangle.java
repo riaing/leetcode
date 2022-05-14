4cff1070f8c4ac5f97b5f91eca8fdc6d598db493
riaing <panjfang@gmail.com>
2022-05-14 15:09:43 -0700

    Update 85. Maximal Rectangle.java

--- 85. Maximal Rectangle.java
+++ 85. Maximal Rectangle.java
@@ -66,3 +66,50 @@ class Solution {
         return area;
     }
 }
+
+--------- 2022.5 常规做法：找两点 --------------------
+  /*
+必须掌握的解法：求matrix中两点围成的长方形的面积
+
+本题变一点：求任意两点围成的长方形中的1的总数。如果此时长*宽 = 1的总数，说明围成的是长方形
+1. 求（0，0）到任意点围成的长方形中1的总数  -> O（n^2)
+dp[i][j] - 以（i.j） 为右下角，到（0，0）的长方形中含有的1的总数
+dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i][j] 
+
+2. 求任一点(i,j) 左上角， 到任一点（a,b）右下角，围成的长方形中1的总数
+size = dp[a][b] - dp[a-i][b] - dp[a, b-j] + dp[a-i][b-j] 
+
+3. 判断这两点是否围成长方形： size == (a-i)*(b-j) 
+*/
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        // 1 - O(n^2)
+        int[][] dp = new int[matrix.length + 1][matrix[0].length + 1]; // 上，左分别加一行0
+        for (int i = 1; i <= matrix.length; i++) {
+            for (int j = 1; j <= matrix[0].length; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] -dp[i-1][j-1] + (matrix[i-1][j-1] - '0');
+                System.out.println("i " + i + " j " + j + " : " + dp[i][j]);
+            }
+        }
+    
+        // 2. 任意点(i,j) 左上角， 到任意点（a,b）右下角，围成的长方形中1的总数
+        //2.1 边界
+        int res = 0; 
+        int size = 0; 
+        // 确定右下角
+        for (int a = 1; a <= matrix.length; a++) {
+           for (int b = 1; b <= matrix[0].length; b++) {
+               // 确定左上角
+               for (int i = a - 1; i >=0; i--) {
+                   for (int j = b - 1; j >= 0; j--) {
+                       size = dp[a][b] - dp[i][b] - dp[a][j] + dp[i][j];
+                       if (size == (a-i) * (b-j)) {
+                           res = Math.max(res, (a-i)*(b-j));
+                       }
+                   }
+               }
+           }
+        }
+        return res; 
+    }
+}
