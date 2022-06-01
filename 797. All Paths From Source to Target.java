9b1399c28027eed96a93e37eb97c79b6eb1eb34a
riaing <panjfang@gmail.com>
2022-05-31 22:31:37 -0700

    Create 797. All Paths From Source to Target.java

---
+++ 797. All Paths From Source to Target.java
@@ -0,0 +1,107 @@
+Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
+
+The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+
+ 
+
+Example 1:
+
+
+Input: graph = [[1,2],[3],[3],[]]
+Output: [[0,1,3],[0,2,3]]
+Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+Example 2:
+
+
+Input: graph = [[4,3,1],[3,2,4],[3],[4],[]]
+Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+ 
+
+Constraints:
+
+n == graph.length
+2 <= n <= 15
+0 <= graph[i][j] < n
+graph[i][j] != i (i.e., there will be no self-loops).
+All the elements of graph[i] are unique.
+The input graph is guaranteed to be a DAG.
+
+---------------------------------------- Recursion -------------------------------------------
+
+/*
+expential time 
+
+DP: 
+"the paths from the current node to the target node consist of all the paths starting from each neighbor of the current node."
+*/
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<Integer> cur = new ArrayList<>();
+        cur.add(0);
+        List<List<Integer>> res = new ArrayList<>();
+        helper(graph, 0, cur, res);
+        return res; 
+    }
+    
+    private void helper(int[][] graph, int index, List<Integer> cur, List<List<Integer>> res) {
+        // if (graph[index].length == 0) { // 可能有的点就是没有 children！ 所以这个判断不对
+        if (index == graph.length - 1) {    
+            res.add(new ArrayList<Integer>(cur));
+            return;
+        }
+        int[] nextNodes = graph[index];
+        for (int n : nextNodes) {
+            cur.add(n);
+            helper(graph, n, cur, res);
+            cur.remove(cur.size() - 1); 
+        }
+    }
+}
+
+/*
+ D& C solution + memorization: 
+- The two approach might have the same asymptotic time complexity. However, in practice the DP approach is slower than the backtracking approach, since we copy the intermediate paths over and over. 
+
+class Solution {
+    private int target;
+    private int[][] graph;
+    private HashMap<Integer, List<List<Integer>>> memo;
+
+    protected List<List<Integer>> allPathsToTarget(int currNode) {
+        // memoization. check the result in the cache first
+        if (memo.containsKey(currNode))
+            return memo.get(currNode);
+
+        List<List<Integer>> results = new ArrayList<>();
+        // base case
+        if (currNode == this.target) {
+            ArrayList<Integer> path = new ArrayList<>();
+            path.add(target);
+            results.add(path);
+            return results;
+        }
+
+        // iterate through the paths starting from each neighbor.
+        for (int nextNode : this.graph[currNode]) {
+            for (List<Integer> path : allPathsToTarget(nextNode)) {
+                ArrayList<Integer> newPath = new ArrayList<>();
+                newPath.add(currNode);
+                newPath.addAll(path);
+                results.add(newPath);
+            }
+        }
+        memo.put(currNode, results);
+        return results;
+    }
+
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+
+        this.target = graph.length - 1;
+        this.graph = graph;
+        this.memo = new HashMap<>();
+
+        return this.allPathsToTarget(0);
+    }
+}
+
+*/ 
