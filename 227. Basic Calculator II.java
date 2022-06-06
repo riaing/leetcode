367aefb58198aa4e02760c06ab2451e6f663ecc5
riaing <panjfang@gmail.com>
2022-06-05 18:27:04 -0700

    Update 227. Basic Calculator II.java

--- 227. Basic Calculator II.java
+++ 227. Basic Calculator II.java
@@ -78,3 +78,81 @@ class Solution {
         return false;
     }
 }
+
+--------------- 如果套 calculator 3 模板，就先预处理下string -----------------------------
+ class Solution {
+    public int calculate(String s) {
+         s = s.replaceAll("\\s", ""); // remove space 
+        // 把数字处理好，把原始string放到 queue里
+        Deque<String> input = new LinkedList<String>(); 
+        String num = "";
+        for (char c : s.toCharArray()) {
+            if (isDigit(c)) {
+                num += c;
+            }
+            else {
+                if (!num.isEmpty()) {
+                    input.addLast(num);
+                    num = "";
+                }
+                input.addLast(c+"");
+            }
+        }
+        if (!num.isEmpty()) {
+            input.add(num);
+        }
+        
+        return helper(input); 
+    }
+    
+    /* 重点是每次遇到符号时，处理的是符号前的数字
+    */
+    private int helper(Deque<String> input) {
+        Deque<Integer> stack = new LinkedList<Integer>(); 
+        String sign = "+";
+        int num = 0; 
+        while (input.size() > 0) {
+            String cur = input.removeFirst(); 
+            // System.out.println(cur); 
+            if (isDigit(cur.charAt(0))) {
+                num = Integer.parseInt(cur);
+            }
+            
+            // 如果不是数字，就是遇到了下一个符号，
+            // 之前的数字和符号就要存进栈中。 重点：之前的！
+            if (!isDigit(cur.charAt(0)) || input.size() == 0){ // 进来的有可能是走到底了，或符号，或括号
+                if (sign.equals("+")) {
+                    stack.push(num);
+                }
+                if (sign.equals("-")) {
+                    stack.push(-num);
+                }
+                if (sign.equals("*")) {
+                    int pre = stack.pop();
+                    stack.push(pre * num); 
+                }
+                if (sign.equals("/")) {
+                    int pre = stack.pop();
+                    stack.push(pre/num);
+                }
+                // 更新到当前的
+                num = 0;
+                sign = cur; 
+            }
+        }
+                   
+        // 最后拿出stack的元素做加
+        int res = 0;
+        while (!stack.isEmpty()) {
+            res += stack.pop();
+        }
+        return res; 
+    }
+    
+    private boolean isDigit(char c) {
+        if (c >= '0' && c <= '9') {
+            return true;
+        }
+        return false;
+    }
+}
