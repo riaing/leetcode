ba98c2378ec4c898fd0a457898b8829762c80ce1
riaing <panjfang@gmail.com>
2022-06-14 22:25:18 -0700

    Update 526. Beautiful Arrangement.java

--- 526. Beautiful Arrangement.java
+++ 526. Beautiful Arrangement.java
@@ -56,4 +56,51 @@ class Solution {
     }
 }
 
--------------------
+------------------- memo by using bit manipulation,最差也是 o(2^n) ------------------
+ /*
+最坏每个state都要访问一遍。 O (2^N)
+*/
+class Solution {
+    public int countArrangement(int n) {
+        int[] dp = new int[1<<n];
+        Arrays.fill(dp, -1); 
+        return helper(n, dp, 0, 1);
+        
+    }
+    private int helper(int n, int[] dp, int state, int index) {
+        // if (state == (1<<n)-1) { // base case的写法1. 每个数字都用了. 可以按照下个
+        //     return 1; 
+        // }
+        
+        if (index == n+1) { // base case的写法2
+            return 1; 
+        }
+        
+        if (dp[state] != -1) {
+            return dp[state];
+        }
+        
+        // get index -> 通过传参数解决了
+        // int index = 1; // 因为1 base
+        // for (int i = 0; i < n; i++) {
+        //     if ((state & (1 << i)) > 0) { // 如果i+1这个数被用过了
+        //         index++; 
+        //     }
+        // }
+        
+        // 计算
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+             if ((state & (1 << i)) > 0) { // 如果i+1这个数被用过了
+                continue; 
+            }
+            // 没被用，计算是否valid
+            if ((i+1) % index == 0 || index % (i+1) == 0) {
+                int newState = state | 1 << i; // 把当前数用上
+                sum += helper(n, dp, newState, index+1); 
+            }
+        }
+        dp[state] = sum;
+        return sum; 
+    }
+}
