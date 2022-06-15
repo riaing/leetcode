4bf88ffd69164b0f5fa8ec265162c0dee2a16d2f
riaing <panjfang@gmail.com>
2022-06-14 18:23:25 -0700

    Create 526. Beautiful Arrangement.java

---
+++ 526. Beautiful Arrangement.java
@@ -0,0 +1,59 @@
+Suppose you have n integers labeled 1 through n. A permutation of those n integers perm (1-indexed) is considered a beautiful arrangement if for every i (1 <= i <= n), either of the following is true:
+
+perm[i] is divisible by i.
+i is divisible by perm[i].
+Given an integer n, return the number of the beautiful arrangements that you can construct.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: 
+The first beautiful arrangement is [1,2]:
+    - perm[1] = 1 is divisible by i = 1
+    - perm[2] = 2 is divisible by i = 2
+The second beautiful arrangement is [2,1]:
+    - perm[1] = 2 is divisible by i = 1
+    - i = 2 is divisible by perm[2] = 1
+Example 2:
+
+Input: n = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 15
+
+---------------------- backtracking 时间差--------------------
+/*
+1. backtracking. (2^n)
+
+
+*/
+class Solution {
+    int res; 
+    public int countArrangement(int n) {
+        Set<Integer> visited = new HashSet<>();
+        helper(n, 1, visited);
+        return res; 
+    }
+    
+    private void helper(int n, int index, Set<Integer> visited) {
+        if (index > n) {
+            res++;
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!visited.contains(i) && (i % index == 0 || index % i == 0)) {
+                visited.add(i);
+                helper(n, index+1, visited);
+                visited.remove(i);
+            }
+        }
+    }
+}
+
+-------------------
