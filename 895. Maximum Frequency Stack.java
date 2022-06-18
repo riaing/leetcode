66bec468f6f0d6638d9a7d56826f7a5d1bbfda57
riaing <panjfang@gmail.com>
2022-06-18 11:58:13 -0700

    Update 895. Maximum Frequency Stack.java

--- 895. Maximum Frequency Stack.java
+++ 895. Maximum Frequency Stack.java
@@ -38,6 +38,60 @@ It is guaranteed that there will be at least one element in the stack before cal
 
 -------------- stack o(1) 写法 -------------------------------------------------
 https://labuladong.gitee.io/algo/2/22/67/ 
+/*
+VF map: val -> freq. 每次push/pop时改
+FV map: freq -> Stack<Val>:  stack保证了tie时返回最新
+maxFreq: 当前的max。
+    - 特性：每次增减by1： 更新时if FVmap的maxFreq为空，则maxFreq--
+重点：不用想着去修改一个元素在 FVMap 中的freqency。eg：【5，5, 4】，FVMap 就会有两行： 1 -> 5 4, 2 -> 5 
+
+Time: O(1)
+Space: O(n)
+*/
+class FreqStack {
+    Map<Integer, Integer> VFmap;
+    Map<Integer, Deque<Integer>> FVmap;
+    int maxFreq; 
+    public FreqStack() {
+        this.VFmap = new HashMap<>();
+        this.FVmap = new HashMap<>();
+        this.maxFreq = 0; 
+    }
+    
+    public void push(int val) {
+        // 1. VF map, freq增加
+        VFmap.put(val, VFmap.getOrDefault(val, 0) + 1); 
+      
+        // 2. FV map
+        int freq = VFmap.get(val);
+        FVmap.putIfAbsent(freq, new LinkedList<>());
+        FVmap.get(freq).push(val);
+        
+        // 3. maxFreq 
+        maxFreq = Math.max(maxFreq, freq);
+    }
+    
+    public int pop() {
+        // 1. 从FV map找到最大freq的node
+        int val = FVmap.get(maxFreq).pop();
+         
+        // 2. 更新VF map
+        VFmap.put(val, VFmap.get(val) - 1); 
+        
+        // 3. 更新 maxFreq
+        if (FVmap.get(maxFreq).isEmpty()) {
+            maxFreq--; 
+        }
+        return val; 
+    }
+}
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(val);
+ * int param_2 = obj.pop();
+ */
 ------------------- Heap 写法 --------------------------------------------
 /*
 方法2:没那么好
