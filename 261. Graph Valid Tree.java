9b601306d5d76bfde238010be0117b3ebfc1bd21
riaing <panjfang@gmail.com>
2022-06-18 22:49:04 -0700

    Update 261. Graph Valid Tree.java

--- 261. Graph Valid Tree.java
+++ 261. Graph Valid Tree.java
@@ -65,6 +65,52 @@ class Solution {
     }
 }
 
+------------------- DFS 更清楚的写法 ----------------
+    class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        // base: 构成图的条件是 V = E + 1 
+        if (n != edges.length + 1) {
+            return false; 
+        }
+        // create graph: O(N + E) N - number of node, E - edges 
+        Map<Integer, Set<Integer>> neibor = new HashMap<Integer, Set<Integer>>(); 
+        for (int i = 0; i < n; i++) { // O(n)
+            neibor.put(i, new HashSet<Integer>());
+        }
+        for (int[] e : edges) { // o(E)
+            int a = e[0];
+            int b = e[1];
+            neibor.get(a).add(b);
+            neibor.get(b).add(a);
+        }
+        
+        // create q and put 0 
+        Set<Integer> visited = new HashSet<Integer>();
+        for (int cur : neibor.keySet()) {
+            if (!visited.contains(cur)) {
+                if (!dfs(cur, -1, visited, neibor)) {
+                    return false;
+                }
+            }
+        }
+        
+        return true; 
+    }
+    
+        // detect cycle 
+    public boolean dfs(int node, int parent, Set<Integer> seen, Map<Integer, Set<Integer>> neibor) {
+        if (seen.contains(node)) return false;
+        seen.add(node);
+        for (int neighbour : neibor.get(node)) {
+            if (parent != neighbour) {
+                boolean result = dfs(neighbour, node, seen, neibor);
+                if (!result) return false;
+            }
+        }
+        return true;
+    }
+}
+
 --------------------2022 常规解法： BFS detect cycle + 查是否union ---------------------------------------
 class Solution {
     public boolean validTree(int n, int[][] edges) {
