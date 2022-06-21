08112bdbb0f43acbe157898f83e9fd08a4a2a040
riaing <panjfang@gmail.com>
2022-06-20 17:27:58 -0700

    Update 151. Reverse Words in a String.java

--- 151. Reverse Words in a String.java
+++ 151. Reverse Words in a String.java
@@ -51,3 +51,15 @@ class Solution {
         return String.join(" ", toReverse);
     }
 }
+
+-------------- 从尾部遍历来reverse  --------------
+ class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]).append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
