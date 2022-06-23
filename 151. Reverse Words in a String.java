a448499416e0e7a24cfc555ad96890d64964553a
riaing <panjfang@gmail.com>
2022-06-23 10:40:55 -0700

    Update 151. Reverse Words in a String.java

--- 151. Reverse Words in a String.java
+++ 151. Reverse Words in a String.java
@@ -33,6 +33,63 @@ There is at least one word in s.
 
 Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
 
+ ----------- 如果是char array的in place reverse -----------------
+ /*
+java 做不到O（1） space，必须得split成array.做法
+1. trim（） -> 遍历string，删除中间多于空格
+2. 遍历char array, 找到每个word，翻转
+3. 翻转整个char array
+
+Time: O(n), space O(n)
+
+*/
+class Solution {
+    public String reverseWords(String s) {
+        // 1. 除掉多于space变成char array
+        s = s.trim(); // remove 首尾space
+        String tmp = "";
+        // 1. 除去中间多于space
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ' && s.charAt(i+1) == ' ') {
+                continue;
+            }
+            tmp += c; 
+        }
+        char[] input = tmp.toCharArray();
+        
+        // 2. in place 翻转。先转每个词，再整个string转
+        int i = 0;
+        while (i < input.length) {
+            if (input[i] == ' ') {
+                i++;
+                continue; 
+            }
+            // find the word's end index 
+            int end = i;
+            while (end < input.length && input[end] != ' ') {
+                end++;
+            }
+            // now end pointer points to the space after the word 
+            reverse(input, i, end - 1); 
+            i = end; 
+        }
+        // 转整个string 
+        reverse(input, 0, input.length-1); 
+        // System.out.println(input);
+        return new String(input); 
+    }
+    
+    private void reverse(char[] input, int start, int end) {
+        while (start < end) {
+            char tmp = input[start];
+            input[start] = input[end];
+            input[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+}
 
 --------------------- CODING -------------------
 /*
